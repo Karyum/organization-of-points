6a59031cf5585e8b2a1358ce9b78d1840f7bcf1a
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2021-01-11 15:44:25 +0200

    lines

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import "./App.css";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import SvgScreen from "./screens/SvgScreen";
 import { useMousePosition } from "./utils/useMousePosition";
 
 function App() {
   const position = useMousePosition();
+  const lineContainer = useRef();
 
-  const line2 = useRef();
+  const [graph, setGraph] = useState({
+    points: [
+      {
+        cx: 1,
+        cy: 1,
+      },
+      {
+        cx: 0,
+        cy: 0,
+      },
+      {
+        cx: 0,
+        cy: 0,
+      },
+      {
+        cx: 0,
+        cy: 0,
+      },
+    ],
+    lines: [
+      { x1: 2, y1: 2, x2: 3, y2: 4 },
+      { x1: 2, y1: 2, x2: 3, y2: 4 },
+      { x1: 2, y1: 2, x2: 3, y2: 4 },
+      { x1: 2, y1: 2, x2: 3, y2: 4 },
+    ],
+  });
+
   const handleLine = (e) => {
-    const id = e.target.id;
-    if (id === "l2") {
-      console.log(line2.current);
-    }
+    console.log("test");
   };
-  const handleCircle = (e) => console.log(e.target.id);
+  const handleCircle = (e) => {
+    // const newLine = document.createElement("line");
+    const newLine = `<line x1="20" y1="20" x2="${position.x}" y2="${position.y}" strokeWidth="4" stroke="black" />`;
+
+    const id = e.target.id;
+    if (id === "c1") {
+      // newLine.current.setAttribute("x1", "20");
+      // newLine.setAttribute("y1", "20");
+      // newLine.setAttribute("x2", "100");
+      // newLine.setAttribute("y2", "100");
+      // newLine.setAttribute("strokeWidth", "4");
+      // newLine.setAttribute("stroke", "black");
+      // lineContainer.current.appendChild(newLine);
+    }
+
+    lineContainer.current.insertAdjacentHTML("beforeend", newLine);
+  };
 
   return (
-    <div className="App">
+    <div className="App" onClick={(e) => console.log(e.clientX, e.clientY)}>
       <SvgScreen
         handleCircle={handleCircle}
         handleLine={handleLine}
-        line2={line2}
+        lineContainer={lineContainer}
       />
       <div style={{ color: "#fff" }}>
         {position.x}:{position.y}
@@ -30,3 +70,16 @@ function App() {
 }
 
 export default App;
+
+{
+  /* <line
+            id="l1"
+            onClick={handleLine}
+            x1="20"
+            y1="20"
+            x2="100"
+            y2="20"
+            strokeWidth="4"
+            stroke="black"
+          /> */
+}
--- src/screens/SvgScreen/index.jsx
+++ src/screens/SvgScreen/index.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 
 function SvgScreen(props) {
-  const { handleCircle, handleLine, line2 } = props;
+  const { handleCircle, handleLine, lineContainer } = props;
   return (
     <div style={{ padding: 10, borderWidth: 1, borderColor: "#000" }}>
       <svg style={{ width: "100%", height: "100vh" }}>
-        <g fill="grey" transform="rotate(120 40 100)">
+        <g fill="grey" transform="rotate(0 0 0)">
           <circle
             id="c1"
             onClick={handleCircle}
             cx="20"
             cy="20"
-            r="6"
+            r="10"
             fill="red"
           />
           <circle
@@ -19,7 +19,7 @@ function SvgScreen(props) {
             onClick={handleCircle}
             cx="100"
             cy="100"
-            r="6"
+            r="10"
             fill="red"
           />
           <circle
@@ -27,7 +27,7 @@ function SvgScreen(props) {
             onClick={handleCircle}
             cx="100"
             cy="20"
-            r="6"
+            r="10"
             fill="red"
           />
           <circle
@@ -35,12 +35,25 @@ function SvgScreen(props) {
             onClick={handleCircle}
             cx="20"
             cy="100"
-            r="6"
+            r="10"
             fill="red"
           />
         </g>
-        <g fill="grey" transform="rotate(120 40 100)">
-          <line
+        <g
+          ref={lineContainer}
+          id="lines-container"
+          fill="grey"
+          transform="rotate(0 40 100)"
+        ></g>
+      </svg>
+    </div>
+  );
+}
+
+export default SvgScreen;
+
+{
+  /* <line
             id="l1"
             onClick={handleLine}
             x1="20"
@@ -51,7 +64,6 @@ function SvgScreen(props) {
             stroke="black"
           />
           <line
-            ref={line2}
             id="l2"
             onClick={handleLine}
             x1="100"
@@ -80,11 +92,38 @@ function SvgScreen(props) {
             y2="100"
             strokeWidth="4"
             stroke="black"
-          />
-        </g>
-      </svg>
-    </div>
-  );
+          /> */
 }
 
-export default SvgScreen;
+//   <circle
+//   id="c1"
+//   onClick={handleCircle}
+//   cx="20"
+//   cy="20"
+//   r="6"
+//   fill="red"
+// />
+// <circle
+//   id="c2"
+//   onClick={handleCircle}
+//   cx="100"
+//   cy="100"
+//   r="6"
+//   fill="red"
+// />
+// <circle
+//   id="c3"
+//   onClick={handleCircle}
+//   cx="100"
+//   cy="20"
+//   r="6"
+//   fill="red"
+// />
+// <circle
+//   id="c4"
+//   onClick={handleCircle}
+//   cx="20"
+//   cy="100"
+//   r="6"
+//   fill="red"
+// />
