48f513e6946d58875013b441810578f427c551d2
Ammar Yusef <amar.yousef@gmail.com>
2021-01-19 15:15:29 +0200

    integrate AnswerField into Question component...

--- src/components/AppTest.js
+++ src/components/AppTest.js
@@ -8,11 +8,11 @@ export default function AppTest() {
   return (
     <QuestionField />
     // <AnswerField
-    //   points={[
-    //     { x: 100, y: 100 },
-    //     { x: 200, y: 100 },
-    //     { x: 200, y: 200 },
-    //   ]}
+      // points={[
+      //   { x: 100, y: 100 },
+      //   { x: 200, y: 100 },
+      //   { x: 200, y: 200 },
+      // ]}
     // />
   );
 }
