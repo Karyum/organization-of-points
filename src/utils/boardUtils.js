4be525e31083ae7aa7b6d4f1f2ce56faab3980a7
Ammar Yusef <amar.yousef@gmail.com>
2021-01-20 11:15:30 +0200

    ajustShapeToBoard function added to board utils file.... see comment within the file....

---
+++ src/utils/boardUtils.js
@@ -0,0 +1,14 @@
+import shapes from "../../public/data.json";
+
+function scalePoint(point, factor) {
+  const { x, y } = point;
+  return { x: x * factor, y: y * factor };
+}
+
+export function adjustShapeToBoard(shape, boardSize) {
+  return shape.map((line) => {
+    const point_1 = scalePoint(line.point_1, boardSize);
+    const point_2 = scalePoint(line.point_2, boardSize);
+    return { point_1, point_2 };
+  });
+}
